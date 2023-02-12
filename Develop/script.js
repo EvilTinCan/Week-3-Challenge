1885bf213f0dabbfa85036e3a7e83db76ca309b9
Tobi <apalac48@gmail.com>
2023-02-12 12:36:11 -0600

    useChars added

--- Develop/script.js
+++ Develop/script.js
@@ -18,14 +18,42 @@ if (pwLength < 8 || pwLength > 128 || isNaN(pwLength)){
   window.alert("Length must be a number between 8 and 128 characters")
   return "WRONG!"
 }
+console.log(pwLength)
+var useLower = window.confirm("Would you like to use lowercase?")
+var useUpper = window.confirm("Would you like to use uppercase?")
+var useNumber = window.confirm("Would you like to use numbers?")
+var useSpecial = window.confirm("Would you like to use special characters?")
+
+var useChars = []
+if (useLower){
+  useChars.push(...lower)
+} 
+if (useUpper){
+  useChars.push(...upper)
+}
+if (useNumber){
+  useChars.push(...number)
+}
+if (useSpecial){
+  useChars.push(...special)
+}
 
 
-
-
-
-
+console.log(useChars)
 
 var pwString = ""
+
+
+for ( var i = 0; i < pwLength; i++){
+  var randomNumber = Math.floor(Math.random() * useChars.length);
+ password += useChar(randomNumber, randomNumber +1);
+}
+
+document.getElementById("password").value = password;
+
+
+
+
   return pwString
 }
 
