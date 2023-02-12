1a217561f7d7b23db55491470a7402ba1697acb7
Tobi <apalac48@gmail.com>
2023-02-12 11:58:37 -0600

    added arrays

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var special = ["!", "@", "#", "$", "%", "&", "*", "?"]
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -9,6 +12,26 @@ function writePassword() {
   passwordText.value = password;
 
 }
+function generatePassword(){
+var pwLength = window.prompt("How long do you want your password to be?");
+if (pwLength < 8 || pwLength > 128 || isNaN(pwLength)){
+  window.alert("Length must be a number between 8 and 128 characters")
+  return "WRONG!"
+}
+
+
+
+
+
+
+
+var pwString = ""
+  return pwString
+}
+
+
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
